a779468540b919bae1ddcd60f4c3183e783d5ba1
Jesse <73019779+jlautenbach@users.noreply.github.com>
2023-05-15 04:47:21 +0200

    Add files via upload

---
+++ RussianToEnglishDictionary.lua
@@ -0,0 +1,23 @@
+ChatTranslateDictionaryRUtoEN = {
+    ["привет"] = "hello",
+    ["да"] = "yes",
+    ["нет"] = "no",
+    ["игра"] = "game",
+    ["игрок"] = "player",
+    ["монстр"] = "monster",
+    ["задание"] = "quest",
+    ["победа"] = "victory",
+    ["поражение"] = "defeat",
+    ["добыча"] = "loot",
+    ["пве"] = "pve",
+    ["пвп"] = "pvp",
+    ["группа"] = "group",
+    ["рейд"] = "raid",
+    ["помощь"] = "help",
+    ["урон"] = "damage",
+    ["исцеление"] = "healing",
+    ["танк"] = "tank",
+    ["лут"] = "loot",
+    ["босс"] = "boss",
+    -- Add more words as needed...
+}
